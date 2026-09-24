1d7976b09905a2b6b5f4a15eaa23974b84e4e871
suedacuvalcii@gmail.com <suedacuvalcii@gmail.com>
2026-09-24 11:21:03 +0000

    sales-ship-product modelleri

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,13 @@
+-- stg_raw__ship.sql
+with source as (
+    select * from {{ source('raw', 'ship') }}
+),
+renamed as (
+    select
+        orders_id,
+        CAST(shipping_fee AS FLOAT64) AS shipping_fee,
+        CAST(logcost AS FLOAT64) AS log_cost,
+        CAST(ship_cost AS FLOAT64) AS ship_cost
+    from source
+)
+select * from renamed
